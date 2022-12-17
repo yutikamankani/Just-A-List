93ca267660314929e9867275dc533ee29389cf49
yutikamankani <97780202+yutikamankani@users.noreply.github.com>
2022-12-17 19:06:26 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+name_array = [];
+
+function newlist(){
+var name_array= document.getElementById("textboxlist").value;
+console.log(name_array);
+document.getElementById("names").innerHTML=name_array;
+}
+  
